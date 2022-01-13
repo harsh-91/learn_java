c8a482cfb3c5b34cd4264c2f8d17d0dc3a9ea17f
tushar <tushar@kryptominds.com>
2022-01-13 17:35:05 +0530

    if statement

---
+++ if_statement.java
@@ -0,0 +1,12 @@
+public class if_statement {
+    public static void main(String[] args) {
+        int myInt = 20;
+
+        if (myInt < 33) {
+            System.out.println("Yes, its True");
+        }
+        else if(myInt == 33) {
+            System.out.println("It's not True");
+        }
+    }
+}
