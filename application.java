b5f3919c17da1736b23a3bc650103cac53b765e3
tushar <tushar@kryptominds.com>
2022-01-13 16:36:59 +0530

    while loop learning

--- application.java
+++ application.java
@@ -1,12 +1,14 @@
 public class application {
-   public static void main(String[] args) {
-        // int myInt = 7;
+    public static void main(String[] args) {
+        int myInt = 7;
         String text = "Hello! its a text ";
         String blank = "";
         String name = "John Doe";
-        String greeting = text+ blank+ name;
+        String greeting = text + blank + name;
 
         System.out.println(greeting);
+        System.out.println("Hello!" + "" + "John Doe");
+        System.out.println("my Integer is " + myInt);
     }
 
 }
---
+++ while_loop.java
@@ -0,0 +1,10 @@
+public class while_loop {
+    public static void main(String[] args) {
+        int value = 0;
+        while(value < 10){
+            System.out.println("Hello! " + value);
+            value = value + 1 ;
+
+        }
+    }
+}
