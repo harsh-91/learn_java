a8775a22cc8941c30915e8cf27d3ce752f006066
tushar <tushar@kryptominds.com>
2022-01-13 15:15:30 +0530

    learn to commit

--- application.java
+++ application.java
@@ -1,6 +1,12 @@
 public class application {
    public static void main(String[] args) {
-        int myInt = 7;
+        // int myInt = 7;
+        String text = "Hello! its a text ";
+        String blank = "";
+        String name = "John Doe";
+        String greeting = text+ blank+ name;
+
+        System.out.println(greeting);
     }
 
 }
