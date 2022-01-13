77dd4fb50462e578f25325d18ca181e1ee9fbe97
tushar <tushar@kryptominds.com>
2022-01-13 15:00:26 +0530

    new changes hello world

--- hello_world.java
+++ hello_world.java
@@ -3,6 +3,15 @@ public class hello_world{
     {
         System.out.println("Hello world");
         int myNumber= 88;
-        System.out.println(myNumber)
+        short myShort = 558;
+        long myLong = 54545434;
+        double myDouble = 26.3554541;
+        boolean myBoolean = false;
+
+        System.out.println(myLong);
+        System.out.println(myShort);
+        System.out.println(myNumber);
+        System.out.println(myDouble);
+        System.out.println(myBoolean);
     }
 }
