99a7df371433d78c908e0e0905eb176bb418946f
tushar <tushar@kryptominds.com>
2022-01-14 13:24:46 +0530

    Practice questions

--- Practice.java
+++ Practice.java
@@ -1,11 +1,22 @@
-public class Practice {
+import java.util.Scanner;
+
+public class Practice
+{
     public static void main(String[] args)
     {
-        int sum = 0;
-        for(int i=1; i<=10; i++)
+        Scanner console = new Scanner(System.in);
+        int num; // To hold number
+        int fact = 1; // To hold factorial
+        
+        System.out.print("Enter any positive integer: ");
+        num = console.nextInt();
+       
+        for(int i=1; i<=num; i++)
         {
-            sum += i;
+            fact *= i;
+            System.out.println(fact);
         }
-        System.out.println("Sum: " + sum);
+        
+        System.out.println("Factorial: "+ fact);
     }
-}
+} 
---
+++ reverse.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+//Write a program that prompts the user to input an integer and then outputs the number with the digits reversed. 
+//For example, if the input is 12345, the output should be 54321.
+
+public class reverse {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        
+        System.out.println("Enter the number you want to reverse : ");
+        int number ;
+        number = console.nextInt();
+        int reverse = 0;
+        while (number != 0) {
+            int remainder = number % 10;
+            reverse = reverse * 10 + remainder;
+            number = number / 10;
+        }
+        System.out.println("The reverse of the given number is: " + reverse);
+    }
+}
