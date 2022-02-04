5e9a274903fa6170caf42341ead85e400e5e6848
tushar <tushar@kryptominds.com>
2022-02-04 15:22:52 +0530

    string builder and formatting

---
+++ string_builder.java
@@ -0,0 +1,13 @@
+public class string_builder {
+    public static void main(String[] args) {
+        String info = "";
+
+        info += "My name is bob";
+        info += "";
+        info += "I am a builder";
+
+
+        System.out.println(info);
+    }
+    
+}
